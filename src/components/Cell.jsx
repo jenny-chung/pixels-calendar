7c269a3a47759263812d2261702ce1d5c7eba3a2
Jenny Chung <jennychung16@gmail.com>
2024-08-07 22:38:22 +0800

    added background colour prop for cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import Circle from './Circle';
+import Pixel from './Pixel';
 
-const Cell = ( { content, className, title, handleClick, isSelected }) => {
+const Cell = ( { content, className, title, colour, handleClick, isToday, isFuture }) => {
+
+  colour = isFuture ? "bg-transparent" : colour;
+
   return (
     <>
     <div
-        className={clsx("h-14 flex items-center justify-center border-b-2 border-r-2", {"cursor-pointer hover:bg-red-100 active:bg-red-200" : handleClick}, className)}
-        onClick={handleClick} title={title}>
-        {isSelected ? <Circle colour="bg-orange-300" content={content}/> : <p>{content}</p>}
-    </div>  
+        className={clsx(`${colour} h-14 flex items-center justify-center border-b-2 border-r-2`, {"cursor-pointer hover:bg-gray-100 active:bg-gray-200" : handleClick && !isFuture}, {"cursor-not-allowed" : isFuture}, className)}
+        onClick={handleClick}
+        title={title}>
+        <p>{content}</p>
+        {/* {isSelected ? <Pixel colour={colour} content={content}/> : <p>{content}</p>} */}
+        {/* {isToday && <Circle colour="bg-orange-200" content={content}/>} */}
+    </div> 
     </>
        
   )
