89c120bbb0b61ab23cba07bce340f489a4b580db
Jenny Chung <jennychung16@gmail.com>
2024-08-07 22:30:02 +0800

    changed navbar margins

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="mt-3 ml-6 flex items-center">
-        <img src="src/orange.png" width="50" height="50" alt="orange logo" />
-        <div className='ml-1 text-xl font-thin'>pixels</div>
+    <div className='my-3 mx-6 flex flex-row items-center'>
+      <img src="src/orange.png" width="50" height="50" alt="orange logo" />
+      <div className='ml-1 text-xl font-thin'>pixels</div> 
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
