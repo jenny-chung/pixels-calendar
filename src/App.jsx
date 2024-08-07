1bcf06c5446c65f1059fc2db3fe252f2f991ee68
Jenny Chung <jennychung16@gmail.com>
2024-08-07 22:19:12 +0800

    changed today button styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const [currentDate, setCurrentDate] = useState(new Date());
 
-  const handleSetDateToday = () => setCurrentDate(new Date());
+  const handleSetToday = () => setCurrentDate(new Date());
 
   return (
     <div>
@@ -16,7 +16,7 @@ function App() {
       <div className='mt-16 flex flex-col items-center gap-4'>
         <div className='flex flex-col items-center gap-2'>
           {/* <p className='text-sm'>Selected Date: {format(currentDate, "LLLL d, yyyy")}</p> */}
-          <button onClick={handleSetDateToday} className='bg-white hover:bg-sky-600/50 active:bg-sky-500/50 border rounded px-4 py-2'>Today</button>
+          <button onClick={handleSetToday} className='bg-pink-900 text-white hover:bg-pink-800 active:bg-pink-700/50 border rounded px-4 py-2'>Today</button>
         </div>
         <Calendar value={currentDate} onChange={setCurrentDate} />
       </div>
