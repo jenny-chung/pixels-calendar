9e65e3078316587b1f47ef30ed99a5c926d9fcee
Jenny Chung <jennychung16@gmail.com>
2024-09-10 14:33:56 +0800

    moved logo picture into assets folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className='my-3 mx-6 flex flex-row items-center'>
-      <img src="src/orange.png" width="50" height="50" alt="orange logo" />
-      <div className='ml-1 text-xl font-thin'>pixels</div> 
+      <img src="src/assets/orange.png" width="50" height="50" alt="logo" />
+      <div className='ml-2 text-xl font-thin'>pixels</div> 
     </div>
   )
 }
