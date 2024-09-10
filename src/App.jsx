f62398c3ee3f5d3075d8edf46081fb438d0de757
Jenny Chung <jennychung16@gmail.com>
2024-09-11 03:26:48 +0800

    added yearly view for calendar

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,34 @@ import { format } from 'date-fns';
 function App() {
 
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [view, setView] = useState('monthly');
 
   const handleSetToday = () => setCurrentDate(new Date());
 
+  const renderYearlyView = () => {
+    return (
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-12'>
+        {Array.from({ length: 12 }).map((_, index) => (
+        <Calendar
+          key={index}
+          view={view}
+          value={new Date(currentDate.getFullYear(), index, 1)}
+          onChange={setCurrentDate} />
+      ))}
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <Navbar />
-      <div className='mt-16 flex flex-col items-center gap-4'>
-        <div className='flex flex-col items-center gap-2'>
-          {/* <p className='text-sm'>Selected Date: {format(currentDate, "LLLL d, yyyy")}</p> */}
-          <button onClick={handleSetToday} className='bg-pink-900 text-white hover:bg-pink-800 active:bg-pink-700/50 border rounded px-4 py-2'>Today</button>
-        </div>
-        <Calendar value={currentDate} onChange={setCurrentDate} />
+    <div className="min-h-screen flex flex-col flex-grow">
+      <Navbar view={view} handleView={setView} />
+      <div className='flex flex-col flex-grow justify-center items-center gap-4 pt-8 pb-16'>
+        {/* <p className='text-sm'>Selected Date: {format(currentDate, "LLLL d, yyyy")}</p> */}
+        {view === 'monthly' && <button onClick={handleSetToday} className='bg-pink-900 text-white hover:bg-pink-800 active:bg-pink-700/50 border rounded px-4 py-2'>Today</button>}
+       
+        {view === 'monthly' ? 
+        (<Calendar view={view} value={currentDate} onChange={setCurrentDate} />) : (renderYearlyView())
+        } 
       </div>
       <Footer />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import clsx from 'clsx';
 
-const Navbar = () => {
+const Navbar = ({ view, handleView }) => {
   return (
-    <div className='my-3 mx-6 flex flex-row items-center'>
+    <div className='my-3 mx-6 flex flex-row items-center justify-between'>
+    <div className='flex items-center'>
       <img src="src/assets/orange.png" width="50" height="50" alt="logo" />
-      <div className='ml-2 text-xl font-thin'>pixels</div> 
+      <div className='ml-2 text-xl font-thin'>pixels</div>
+    </div>
+    
+    <div>
+      <button onClick={() => handleView('monthly')} className='ml-auto bg-orange-300 text-white px-3 py-2 border rounded hover:bg-orange-400'>Month</button>
+      <button onClick={() => handleView('yearly')} className='ml-2 bg-orange-300 text-white px-3 py-2 border rounded hover:bg-orange-400'>Year</button>
+    </div>
+    
     </div>
   )
 }
