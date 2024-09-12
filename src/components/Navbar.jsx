44ff831f49b60046cc36b6eac47e9721b13c1f44
Jenny Chung <jennychung16@gmail.com>
2024-09-12 19:10:47 +0800

    changed to importing logo instead of using path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Orange from '../assets/orange.png';
 
 const getButtonClass = (currentView, view) => {
   return `ml-2 px-3 py-2 text-white border rounded hover:bg-rose-400 ${currentView === view ? 'bg-pink-700' : 'bg-rose-300'}`;
@@ -9,7 +10,7 @@ const Navbar = ({ view, handleView }) => {
   return (
     <div className='my-3 mx-6 flex flex-row items-center justify-between'>
     <div className='flex items-center'>
-      <img src="src/assets/orange.png" width="50" height="50" alt="logo" />
+      <img src={Orange} width="50" height="50" alt="logo" />
       <div className='ml-2 text-xl font-thin'>pixels</div>
     </div>
    
