7267a6d71af07bd961223f1a5229fd75c8af2c3d
Jenny Chung <jennychung16@gmail.com>
2024-09-12 18:45:56 +0800

    minor renaming and refactoring to PixelModal component

--- src/components/PixelModal.jsx
+++ src/components/PixelModal.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { format } from 'date-fns';
-import PixelColour, { palette_1 } from './PixelColour';
+import PixelColours, { default_palette } from './PixelColour';
 
-// Pixel modal for choosing mood of day 
+// Pixel modal for choosing mood for specific day 
 const PixelModal = ({ showModal, moods, onChange, selectedDate }) => {
 
     const [selectedMood, setSelectedMood] = useState(null);
 
-    const handleColourChange = (index) => {
-        setSelectedMood(palette_1[index].color);
+    const handleMoodSelection = (index) => {
+        setSelectedMood(default_palette[index].colour);
     }
 
     const handleSave = () => {
@@ -20,6 +20,10 @@ const PixelModal = ({ showModal, moods, onChange, selectedDate }) => {
             console.log({updatedMoods});
             onChange(updatedMoods);
         }
+        closeModal();
+    }
+
+    const closeModal = () => {
         showModal(false);
     }
 
@@ -29,7 +33,7 @@ const PixelModal = ({ showModal, moods, onChange, selectedDate }) => {
 
             {/* Close Popup Button */}
             <div className='flex justify-end p-2'>
-                <button className='bg-white hover:bg-slate-200 px-3 py-1 border rounded' onClick={() => showModal(false)}>X</button>
+                <button className='bg-white hover:bg-slate-200 px-3 py-1 rounded' onClick={closeModal}>X</button>
             </div>
 
             <div className='p-2 text-center text-xl'> 
@@ -37,12 +41,11 @@ const PixelModal = ({ showModal, moods, onChange, selectedDate }) => {
                 <p className='text-sm'>Selected Date: {format(selectedDate, "LLLL d, yyyy")}</p>
             </div>
 
-            {/* Pixel Colours */}
-            <PixelColour handleColourChange={handleColourChange} />
+            <PixelColours handleMoodSelection={handleMoodSelection} />
                 
             <div className='p-6 pt-0 text-center'>
-                <button className='text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-slate-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center' onClick={handleSave}>Save</button>
-                <button className='ml-4 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2' onClick={() => showModal(false)}>Cancel</button>
+                <button className='text-white bg-teal-500 hover:bg-teal-800 focus:ring-4 focus:ring-teal-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center' onClick={handleSave}>Save</button>
+                <button className='ml-3 mr-2 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center' onClick={closeModal}>Cancel</button>
             </div>
 
         </div>
