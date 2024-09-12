dadc13db97298980a80131a5bf3ef693fbe286f9
Jenny Chung <jennychung16@gmail.com>
2024-09-12 17:44:10 +0800

    added monthly and yearly view buttons to navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import clsx from 'clsx';
+
+const getButtonClass = (currentView, view) => {
+  return `ml-2 px-3 py-2 text-white border rounded hover:bg-rose-400 ${currentView === view ? 'bg-pink-700' : 'bg-rose-300'}`;
+}
 
 const Navbar = ({ view, handleView }) => {
+
   return (
     <div className='my-3 mx-6 flex flex-row items-center justify-between'>
     <div className='flex items-center'>
       <img src="src/assets/orange.png" width="50" height="50" alt="logo" />
       <div className='ml-2 text-xl font-thin'>pixels</div>
     </div>
-    
+   
     <div>
-      <button onClick={() => handleView('monthly')} className='ml-auto bg-orange-300 text-white px-3 py-2 border rounded hover:bg-orange-400'>Month</button>
-      <button onClick={() => handleView('yearly')} className='ml-2 bg-orange-300 text-white px-3 py-2 border rounded hover:bg-orange-400'>Year</button>
+      <button onClick={() => handleView('monthly')} className={getButtonClass(view, 'monthly')}>Month</button>
+      <button onClick={() => handleView('yearly')} className={getButtonClass(view, 'yearly')}>Year</button>
     </div>
     
     </div>
